10098c7b6d7f25443e302481c14f68f48256a3ad
bigrice0102 <yilinyeh0102@gmail.com>
2017-01-11 17:31:04 +0800

    project structure setting keep testing

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 const express = require('express');
+const routes = require('./routes/routes');
 
 const app = express();
 
-// whatch for incoming request of method GET to the route : http://localhost:3050/api
-
-app.get('/api', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
-app.post('/api', (req, res) => {
-
-});
-
-app.put('/api', (req, res) => {
-
-});
-
-app.delete('/api', (req, res) => {
-
-});
+routes(app);
 
 module.exports = app;
